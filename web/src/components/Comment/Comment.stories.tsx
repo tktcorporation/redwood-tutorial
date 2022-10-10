5002e2cd9cf3cb835a6e728d87ab9d7d9a104bf2
tkt <37575408+tktcorporation@users.noreply.github.com>
2022-10-10 13:20:23 +0000

    :sparkles: finish https://redwoodjs.com/docs/tutorial/chapter7/rbac#mocking-currentuser-for-storybook

--- web/src/components/Comment/Comment.stories.tsx
+++ web/src/components/Comment/Comment.stories.tsx
@@ -1,14 +1,39 @@
 import Comment from './Comment'
 
-export const generated = () => {
+export const defaultView = () => {
   return (
-    <Comment
-      comment={{
-        name: 'Rob Cameron',
-        body: 'This is the first comment!',
-        createdAt: '2020-01-01T12:34:56Z',
-      }}
-    />
+    <div className="m-4">
+      <Comment
+        comment={{
+          id: 1,
+          name: 'Rob Cameron',
+          body: 'This is the first comment!',
+          createdAt: '2020-01-01T12:34:56Z',
+          postId: 1,
+        }}
+      />
+    </div>
+  )
+}
+
+export const moderatorView = () => {
+  mockCurrentUser({
+    roles: 'moderator',
+    id: 1,
+    email: 'moderator@moderator.com',
+  })
+  return (
+    <div className="m-4">
+      <Comment
+        comment={{
+          id: 1,
+          name: 'Rob Cameron',
+          body: 'This is the first comment!',
+          createdAt: '2020-01-01T12:34:56Z',
+          postId: 1,
+        }}
+      />
+    </div>
   )
 }
 
