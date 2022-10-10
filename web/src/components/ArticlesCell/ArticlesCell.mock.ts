d5468993c7cce63d14321450a6f269bb48d9d058
tkt <37575408+tktcorporation@users.noreply.github.com>
2022-10-10 09:26:29 +0000

    :sparkles:; finish https://redwoodjs.com/docs/tutorial/chapter5/first-test#whats-the-deal-with-mocks

---
+++ web/src/components/ArticlesCell/ArticlesCell.mock.ts
@@ -0,0 +1,16 @@
+export const standard = () => ({
+  articles: [
+    {
+      id: 1,
+      title: 'First Post',
+      body: `Neutra tacos hot chicken prism raw denim...`,
+      createdAt: '2020-01-01T12:34:56Z',
+    },
+    {
+      id: 2,
+      title: 'Second Post',
+      body: `Master cleanse gentrify irony put a bird on it...`,
+      createdAt: '2020-01-01T12:34:56Z',
+    },
+  ],
+})
