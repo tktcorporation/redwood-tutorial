0ff871b872af190dd401219502d3cb9bf9c2dd8b
tkt <37575408+tktcorporation@users.noreply.github.com>
2022-10-10 11:02:39 +0000

    :sparkles: finish https://redwoodjs.com/docs/tutorial/chapter6/comment-form#submitting

--- web/src/components/CommentForm/CommentForm.stories.tsx
+++ web/src/components/CommentForm/CommentForm.stories.tsx
@@ -1,21 +1,29 @@
-// When you've added props to your component,
-// pass Storybook's `args` through this story to control it from the addons panel:
-//
-// ```tsx
-// import type { ComponentStory } from '@storybook/react'
-//
-// export const generated: ComponentStory<typeof CommentForm> = (args) => {
-//   return <CommentForm {...args} />
-// }
-// ```
-//
-// See https://storybook.js.org/docs/react/writing-stories/args.
-
 import type { ComponentMeta } from '@storybook/react'
+import type {
+  CreateCommentMutation,
+  CreateCommentMutationVariables,
+} from 'types/graphql'
 
 import CommentForm from './CommentForm'
 
 export const generated = () => {
+  mockGraphQLMutation<CreateCommentMutation, CreateCommentMutationVariables>(
+    'CreateCommentMutation',
+    (variables, { ctx }) => {
+      const id = Math.floor(Math.random() * 1000)
+      ctx.delay(1000)
+
+      return {
+        createComment: {
+          id,
+          name: variables.input.name,
+          body: variables.input.body,
+          createdAt: new Date().toISOString(),
+        },
+      }
+    }
+  )
+
   return <CommentForm />
 }
 
--- web/src/components/CommentForm/CommentForm.tsx
+++ web/src/components/CommentForm/CommentForm.tsx
@@ -1,8 +1,77 @@
+import {
+  Form,
+  FormError,
+  Label,
+  TextField,
+  TextAreaField,
+  Submit,
+  SubmitHandler,
+} from '@redwoodjs/forms'
+import { useMutation } from '@redwoodjs/web'
+
+const CREATE = gql`
+  mutation CreateCommentMutation($input: CreateCommentInput!) {
+    createComment(input: $input) {
+      id
+      name
+      body
+      createdAt
+    }
+  }
+`
+
+interface FormValues {
+  name: string
+  comment: string
+}
+
 const CommentForm = () => {
+  const [createComment, { loading, error }] = useMutation(CREATE)
+
+  const onSubmit: SubmitHandler<FormValues> = (input) => {
+    createComment({ variables: { input } })
+  }
+
   return (
     <div>
-      <h2>{'CommentForm'}</h2>
-      <p>{'Find me in ./web/src/components/CommentForm/CommentForm.tsx'}</p>
+      <h3 className="font-light text-lg text-gray-600">Leave a Comment</h3>
+      <Form className="mt-4 w-full" onSubmit={onSubmit}>
+        <FormError
+          error={error}
+          titleClassName="font-semibold"
+          wrapperClassName="bg-red-100 text-red-900 text-sm p-3 rounded"
+        />
+        <Label
+          name="name"
+          className="block text-xs font-semibold text-gray-500 uppercase"
+        >
+          Name
+        </Label>
+        <TextField
+          name="name"
+          className="block w-full p-1 border rounded text-sm "
+          validation={{ required: true }}
+        />
+
+        <Label
+          name="body"
+          className="block mt-4 text-xs font-semibold text-gray-500 uppercase"
+        >
+          Comment
+        </Label>
+        <TextAreaField
+          name="body"
+          className="block w-full p-1 border rounded h-24 text-sm"
+          validation={{ required: true }}
+        />
+
+        <Submit
+          disabled={loading}
+          className="block mt-4 bg-blue-500 text-white text-xs font-semibold uppercase tracking-wide rounded px-3 py-2 disabled:opacity-50"
+        >
+          Submit
+        </Submit>
+      </Form>
     </div>
   )
 }
